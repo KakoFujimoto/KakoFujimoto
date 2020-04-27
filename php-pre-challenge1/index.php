9673311845160eef7386489c3b592dc595e48dc3
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-04-27 17:49:45 +0900

    インデント調整

--- php-pre-challenge1/index.php
+++ php-pre-challenge1/index.php
@@ -2,11 +2,11 @@
 for ($i =1; $i <= 100; $i++){
         if ($i % 15 === 0){
             echo "3の倍数であり、5の倍数<br>";
-        }   elseif  ($i % 3 === 0){
+        } elseif ($i % 3 === 0){
             echo "3の倍数<br>";
-        }   elseif  ($i % 5 === 0){
+        } elseif ($i % 5 === 0){
             echo "5の倍数<br>";
-        }   else    {
+        } else {
             echo "$i<br>";
         }
-    }
+        }
